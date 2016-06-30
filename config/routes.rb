5ffcf7eccad27f5b9a870dc530bc111fe0e39907
Jessica Lee <jessicayungalee@gmail.com>
2016-06-30 03:56:36 +0000

    Added enrollments to database with model and controller. Belongs to user. Can now enroll on course pages, should prevent users from enrolling twice.

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,9 @@ Flixter::Application.routes.draw do
   #   end
 
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
